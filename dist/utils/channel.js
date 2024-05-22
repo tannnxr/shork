02eb80ace18db06cb762b7f1522d996caa18009b
tanner@ubuntu <tannnxr@gmail.com>
2024-05-22 16:16:18 -0400

    removed build

--- dist/utils/channel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getChannel = void 0;
-const getChannel = (client, channelId) => {
-    return client.channels.cache.filter((channel) => channel.id == channelId).get(channelId);
-};
-exports.getChannel = getChannel;
--- dist/utils/commands.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCommandFiles = void 0;
-function getCommandFiles() {
-    console.log(__dirname);
-}
-exports.getCommandFiles = getCommandFiles;
