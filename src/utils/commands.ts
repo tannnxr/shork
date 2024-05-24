7a0df3344cfb281585582021f79ddffaffccf67e
tanner@ubuntu <tannnxr@gmail.com>
2024-05-23 21:43:41 -0400

    Added the use of the logger util + checking workingDir

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,5 +1,10 @@
 import path from "path";
+import { LogType, Logger } from "./logging";
+
+const logger = new Logger(__filename, LogType.DEBUG)
+
 
 export function getCommandFiles() {
-    console.log(__dirname)
+    const workingDir = __filename;
+	logger.log(`${workingDir}`, LogType.DEBUG)
 }
