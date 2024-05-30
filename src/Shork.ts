6492cf0aa172952184343639988c63fa3f0f782b
Tanner <tannnxr@gmail.com>
2024-05-30 19:39:09 -0400

    refactor: setup command registration and changed return type on Event register

--- src/Shork.ts
+++ src/Shork.ts
@@ -1,24 +1,64 @@
 import { Client, ClientOptions, PermissionFlagsBits } from "discord.js";
 import { registerEvents } from "./events/register";
+import { getCommandFiles } from "./utils/commands";
+import { CommandReferenceType } from './registerCommands';
 
+import { Logger, LogType } from "./utils/logging";
 
 class Shork extends Client {
-	eventsRegistered: number;
+	eventsRegistered: string[];
+	commandsRegistered: CommandReferenceType[];
+	logger: Logger
 	constructor (options: ClientOptions) {
 		super(options);
-		this.eventsRegistered = 0;
 
-		this.registerEvents()
+		this.logger = new Logger(__filename, LogType.DEBUG)
+		this.eventsRegistered = [];
+		this.commandsRegistered = [];
+
+		this.registerEvents();
+		this.registerCommands();
 
 		return this
 	}
 
-	registerEvents() {
-		registerEvents(this)
+	async registerEvents() {
+		const eventsOr = await registerEvents(this)
+		if (eventsOr == false) {
+			this.logger.log(`Events unable to be registered for some reason.`, LogType.ERROR)
+			throw new Error("Fatal error: Events not registered.")
+		}
+
+		this.eventsRegistered = eventsOr as string[];
+
+	}
+
+	async registerCommands() {
+		const commandFiles = await getCommandFiles()
+
+		commandFiles.forEach(async (file) => {
+			this.logger.log(file)
+			const cmdFile = await import(file);
+			const importedCmd = cmdFile.default as CommandReferenceType;
+			console.log(importedCmd)
+			if (importedCmd.data !== null && importedCmd.execute !== null) {
+				this.commandsRegistered.push(importedCmd);
+			}
+		})
+
+		this.logger.log(`Commands Registered (${this.commandsRegistered.length})`);
 	}
 
 	getRegisteredEvents() {
-		return this.eventsRegistered;
+		return {events: this.eventsRegistered, count: this.eventsRegistered.length}
+	}
+
+	getRegisteredCommands() {
+		const commands: string[] = []
+		this.commandsRegistered.forEach((command) => {
+			commands.push(command.data.name)
+		})
+		return {commands: commands, count: commands.length}
 	}
 }
 
