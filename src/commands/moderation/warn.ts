341137db5b401b51f359636fcd594b1f57c6e398
tannnxr <tannnxr@gmail.com>
2024-05-30 22:13:17 -0400

    add: redis cache added for warnings

--- src/commands/moderation/warn.ts
+++ src/commands/moderation/warn.ts
@@ -1,33 +1,46 @@
 import {
-  ChatInputCommandInteraction,
-  PermissionFlagsBits,
-  SlashCommandBuilder,
-  SlashCommandMentionableOption,
-  SlashCommandStringOption,
+	ChatInputCommandInteraction,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+	SlashCommandMentionableOption,
+	SlashCommandStringOption,
 } from "discord.js";
 import { LogType, Logger } from "../../utils/logging";
 
+import { shorkCache } from "../../db/cache";
+
 export default {
-  data: new SlashCommandBuilder()
-    .setName("warn")
-    .setDescription("Warn a user.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
-    .addMentionableOption((option) =>
-      option
-        .setName("user")
-        .setDescription("The user to warn.")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("reason")
-        .setDescription("The reason you're warning the user for")
-        .setRequired(false)
-    ),
+	data: new SlashCommandBuilder()
+		.setName("warn")
+		.setDescription("Warn a user.")
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+		.addMentionableOption((option) =>
+			option
+				.setName("user")
+				.setDescription("The user to warn.")
+				.setRequired(true)
+		)
+		.addStringOption((option) =>
+			option
+				.setName("reason")
+				.setDescription("The reason you're warning the user for")
+				.setRequired(false)
+	)
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
-  async execute(interaction: ChatInputCommandInteraction) {
-    const logger = new Logger(__filename, LogType.DEBUG);
+	async execute(interaction: ChatInputCommandInteraction) {
+		const logger = new Logger(__filename, LogType.DEBUG);
 
-    logger.log("warn command");
-  },
+		const options = interaction.options;
+		const member = options.getMentionable("user");
+		const reason = options.getString("reason");
+
+		if (await shorkCache.hGetAll('warnings')) { 
+			await shorkCache.hSet('warnings', {})
+		}
+
+		const warnings = await shorkCache.hGetAll('warnings')
+
+		console.log(warnings)
+	},
 };
