a4323e5486a2eb471e18b53eed6b62e8b2c74328
tannnxr <tannnxr@gmail.com>
2024-05-30 22:14:12 -0400

    update: added actual command execution

--- src/events/interactions/interactionCreate.ts
+++ src/events/interactions/interactionCreate.ts
@@ -1,8 +1,9 @@
 import {
-  BaseInteraction,
-  Events,
-  InteractionType,
-  TextChannel,
+	BaseInteraction,
+	ChatInputCommandInteraction,
+	Events,
+	InteractionType,
+	TextChannel,
 } from "discord.js";
 import { Shork } from "../../Shork";
 import { getChannel } from "../../utils/channel";
@@ -12,10 +13,23 @@ import { CommandReferenceType } from "../../registerCommands";
 import { getCommandFiles } from "../../utils/commands";
 
 export default {
-  name: Events.InteractionCreate,
-  execute: async (interaction: BaseInteraction) => {
-    const client = interaction.client as Shork;
-    if (interaction.type == InteractionType.ApplicationCommand) {
-    }
-  },
+	name: Events.InteractionCreate,
+	execute: async (interaction: BaseInteraction) => {
+		const client = interaction.client as Shork;
+		if (interaction.type == InteractionType.ApplicationCommand) {
+			const chatInteraction = interaction as ChatInputCommandInteraction;
+			const registeredCommands = await client.getRegisteredCommands();
+			if (registeredCommands.count < 1) {
+				(interaction as ChatInputCommandInteraction).reply(
+					"Looks like there aren't any commands registered!!!"
+				);
+			}
+
+			registeredCommands.commands.forEach((command: CommandReferenceType) => {
+				if (chatInteraction.commandName === command.data.name) {
+					command.execute(chatInteraction);
+				}
+			});
+		}
+	},
 };
