664144dccc18d75356a027a281f9be00c210e4f8
tannnxr <tannnxr@gmail.com>
2024-05-30 22:12:35 -0400

    update + patch: fixed register commands and getter from returning nothing

--- src/Shork.ts
+++ src/Shork.ts
@@ -1,65 +1,77 @@
 import { Client, ClientOptions, PermissionFlagsBits } from "discord.js";
 import { registerEvents } from "./events/register";
 import { getCommandFiles } from "./utils/commands";
-import { CommandReferenceType } from './registerCommands';
+import { CommandReferenceType } from "./registerCommands";
 
 import { Logger, LogType } from "./utils/logging";
+
+// Import custom errors
+
+import { InvalidTokenError, EventRegistrationError } from "./Errors";
 
 class Shork extends Client {
 	eventsRegistered: string[];
 	commandsRegistered: CommandReferenceType[];
-	logger: Logger
-	constructor (options: ClientOptions) {
+	logger: Logger;
+	constructor(options: ClientOptions) {
 		super(options);
 
-		this.logger = new Logger(__filename, LogType.DEBUG)
+		this.logger = new Logger(__filename, LogType.DEBUG);
 		this.eventsRegistered = [];
 		this.commandsRegistered = [];
-
-		this.registerEvents();
-		this.registerCommands();
-
-		return this
+		return this;
 	}
 
 	async registerEvents() {
-		const eventsOr = await registerEvents(this)
+		const eventsOr = await registerEvents(this);
 		if (eventsOr == false) {
-			this.logger.log(`Events unable to be registered for some reason.`, LogType.ERROR)
-			throw new Error("Fatal error: Events not registered.")
+			this.logger.log(
+				`Events unable to be registered for some reason.`,
+				LogType.ERROR
+			);
+			throw new EventRegistrationError("Events failed to register.");
 		}
-
 		this.eventsRegistered = eventsOr as string[];
-
 	}
 
 	async registerCommands() {
-		const commandFiles = await getCommandFiles()
-
-		commandFiles.forEach(async (file) => {
-			this.logger.log(file)
+		const commandFiles = await getCommandFiles();
+		for (const file of commandFiles) {
+			this.logger.log(file);
 			const cmdFile = await import(file);
 			const importedCmd = cmdFile.default as CommandReferenceType;
-			console.log(importedCmd)
-			if (importedCmd.data !== null && importedCmd.execute !== null) {
-				this.commandsRegistered.push(importedCmd);
+			if (importedCmd.data !== undefined && importedCmd.execute !== null) {
+				this.commandsRegistered.push(importedCmd); // Correctly push the command
 			}
-		})
-
-		this.logger.log(`Commands Registered (${this.commandsRegistered.length})`);
+		}
+		this.logger.log(`Commands Registered`, LogType.SUCCESS);
 	}
 
 	getRegisteredEvents() {
-		return {events: this.eventsRegistered, count: this.eventsRegistered.length}
+		return {
+			events: this.eventsRegistered,
+			count: this.eventsRegistered.length,
+		};
 	}
 
-	getRegisteredCommands() {
-		const commands: string[] = []
-		this.commandsRegistered.forEach((command) => {
-			commands.push(command.data.name)
-		})
-		return {commands: commands, count: commands.length}
+	async getRegisteredCommands() {
+		return { commands: this.commandsRegistered, count: this.commandsRegistered.length };
+	}
+
+	async start(): Promise<boolean> {
+		const token = process.env.TOKEN;
+		if (token === undefined) throw new InvalidTokenError("Check environment variables.");
+		this.logger.log("Shork starting", LogType.DEBUG);
+		try {
+			await this.registerCommands();
+			await this.registerEvents();
+			await this.login(token);
+			return true;
+		} catch (error) {
+			this.logger.log(`Error with starting: ${error}`, LogType.ERROR);
+			return false;
+		}
 	}
 }
 
-export {Shork};
+export { Shork };
